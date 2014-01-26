af724ea882dcd13b7cb3bc94515f7f69b5dddec3
benny-z <bennyn@wearezeta.com>
2014-01-26 13:48:29 +0100

    Pagination über URL. BLOG

--- src/main/java/de/fhb/repository/AuthorRepository.java
+++ src/main/java/de/fhb/repository/AuthorRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 @Stateless
 public class AuthorRepository {
@@ -18,9 +17,15 @@ public class AuthorRepository {
     return em.createNamedQuery("Author.getCount", Long.class).getSingleResult().intValue();
   }
 
-  public List<Author> getCategories() {
-    TypedQuery<Author> query = em.createNamedQuery("Author.findAll", Author.class);
-    return query.getResultList();
+  public List<Author> getAuthors() {
+    return em.createNamedQuery("Author.findAll", Author.class).getResultList();
+  }
+
+  public List<Author> getAuthors(int offset, int amount) {
+    return em.createNamedQuery("Author.findAll", Author.class)
+            .setFirstResult(offset)
+            .setMaxResults(amount)
+            .getResultList();
   }
 
   public void save(Author author) {
--- src/main/java/de/fhb/service/AuthorService.java
+++ src/main/java/de/fhb/service/AuthorService.java
@@ -3,22 +3,65 @@ package de.fhb.service;
 import de.fhb.model.Author;
 import de.fhb.repository.AuthorRepository;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
-@Named
 @RequestScoped
+@Named
 public class AuthorService {
+
+  private int offset = 0;
+  private int amount = 0;
+  private static final Logger LOG = Logger.getLogger(AuthorService.class.getName());
 
   @EJB
   private AuthorRepository authorRepository;
 
-  public List<Author> getAuthors() {
-    return authorRepository.getCategories();
+  @PostConstruct
+  public void init() {
+    Map<String, String> requestParameterMap = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+
+    String offsetParam = requestParameterMap.get("offset");
+    String amountParam = requestParameterMap.get("amount");
+
+    this.offset = (offsetParam == null) ? 0 : Integer.valueOf(offsetParam);
+    this.amount = (amountParam == null) ? 0 : Integer.valueOf(amountParam);
   }
-  
+
+  public List<Author> getAuthors() {
+    Object[] logValues = new Object[]{
+      this.offset,
+      this.amount
+    };
+    LOG.log(Level.INFO, "Offset: {0}, Amount: {1}", logValues);
+    return authorRepository.getAuthors(this.offset, this.amount);
+  }
+
   public long getAuthorCount() {
     return authorRepository.getAuthorCount();
   }
+
+  /* VIEW PARAMS */
+  public int getOffset() {
+    return offset;
+  }
+
+  public void setOffset(int offset) {
+    this.offset = offset;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
 }
