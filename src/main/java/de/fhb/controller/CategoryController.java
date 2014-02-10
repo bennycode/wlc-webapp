cf11382cdb19e94b4c5ca89ee677684664ce6cb1
benny-z <bennyn@wearezeta.com>
2014-02-10 01:32:43 +0100

    Kategorien können gespeichert werden. Generischer Ansatz wird benötigt!

---
+++ src/main/java/de/fhb/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package de.fhb.controller;
+
+import de.fhb.navigation.Pages;
+import de.fhb.model.Author;
+import de.fhb.repository.AuthorRepository;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named
+public class AuthorController implements Serializable {
+
+  @EJB
+  private AuthorRepository authorRepository;
+
+  private static final Logger LOG = Logger.getLogger(AuthorController.class.getName());
+
+  private Author author;
+
+  @PostConstruct
+  public void init() {
+    this.author = new Author();
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
+  }
+
+  public String save() {
+    // Log
+    String template = "Saving author: {0}";
+    LOG.log(Level.INFO, template, author.getName());
+    // Save
+    this.authorRepository.save(author);
+    this.author = new Author();
+    // Navigate
+    return Pages.ADMIN_AUTHORS;
+  }
+
+  public String delete() {
+    // Log
+    String template = "Deleting author: {0}";
+    LOG.log(Level.INFO, template, author.getName());
+    // Save
+    this.authorRepository.delete(author);
+    this.author = new Author();
+    // Navigate
+    return Pages.ADMIN_AUTHORS;
+  }
+}
