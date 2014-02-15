8f5e2ef802ba6fb35528eaaf50aec1ca00cac28b
benny-z <bennyn@wearezeta.com>
2014-02-15 18:35:24 +0100

    Renamed "autoren" to "authors"

--- src/main/java/de/fhb/navigation/Pages.java
+++ src/main/java/de/fhb/navigation/Pages.java
@@ -3,6 +3,6 @@ package de.fhb.navigation;
 public class Pages {
 
   public static final String ADMIN_INDEX = "/admin/index";
-  public static final String ADMIN_AUTHORS = "/admin/autoren";
+  public static final String ADMIN_AUTHORS = "/admin/authors";
   public static final String ADMIN_CATEGORY = "/admin/categories";
 }
