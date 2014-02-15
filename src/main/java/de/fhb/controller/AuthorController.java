99b0f58f71be79f9467b7871f58c2c10b416c02e
benny-z <bennyn@wearezeta.com>
2014-02-15 19:42:28 +0100

    Added nb-configurations.xml

--- src/main/java/de/fhb/controller/AuthorController.java
+++ src/main/java/de/fhb/controller/AuthorController.java
@@ -14,37 +14,37 @@ import javax.inject.Named;
 @Named
 public class AuthorController extends BaseController<Author> {
 
-	private static final Logger LOG = Logger.getLogger(AuthorController.class.getName());
+  private static final Logger LOG = Logger.getLogger(AuthorController.class.getName());
 
-	@EJB
-	private AuthorService service;
+  @EJB
+  private AuthorService service;
 
-	@PostConstruct
-	public void init() {
-		this.item = new Author();
-	}
+  @PostConstruct
+  public void init() {
+    this.item = new Author();
+  }
 
-	@Override
-	public String edit() {
-		// Log
-		String template = "Saving author: {0}";
-		LOG.log(Level.INFO, template, item.getName());
-		// Save
-		this.service.edit(item);
-		this.item = new Author();
-		// Navigate
-		return Pages.ADMIN_AUTHORS;
-	}
+  @Override
+  public String edit() {
+    // Log
+    String template = "Saving author: {0}";
+    LOG.log(Level.INFO, template, item.getName());
+    // Save
+    this.service.edit(item);
+    this.item = new Author();
+    // Navigate
+    return Pages.ADMIN_AUTHORS;
+  }
 
-	@Override
-	public String remove() {
-		// Log
-		String template = "Deleting author: {0}";
-		LOG.log(Level.INFO, template, item.getName());
-		// Save
-		this.service.remove(item);
-		this.item = new Author();
-		// Navigate
-		return Pages.ADMIN_AUTHORS;
-	}
+  @Override
+  public String remove() {
+    // Log
+    String template = "Deleting author: {0}";
+    LOG.log(Level.INFO, template, item.getName());
+    // Save
+    this.service.remove(item);
+    this.item = new Author();
+    // Navigate
+    return Pages.ADMIN_AUTHORS;
+  }
 }
