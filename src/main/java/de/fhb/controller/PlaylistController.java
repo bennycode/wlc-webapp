ced444e23157023d23c80f3e8d797a66dcb52b08
benny-z <bennyn@wearezeta.com>
2014-02-16 13:32:43 +0100

    Removed Converter

--- src/main/java/de/fhb/controller/PlaylistController.java
+++ src/main/java/de/fhb/controller/PlaylistController.java
@@ -1,8 +1,12 @@
 package de.fhb.controller;
 
+import de.fhb.entities.Author;
+import de.fhb.entities.Category;
 import de.fhb.entities.Playlist;
 import de.fhb.entities.Video;
 import de.fhb.navigation.Pages;
+import de.fhb.service.AuthorService;
+import de.fhb.service.CategoryService;
 import de.fhb.service.PlaylistService;
 import de.fhb.service.YouTubeCrawlerService;
 import javax.annotation.PostConstruct;
@@ -16,8 +20,18 @@ public class PlaylistController extends BaseController<Playlist, PlaylistService
 
   @EJB
   private PlaylistService service;
+
   @EJB
-  private YouTubeCrawlerService ytService;
+  private AuthorService authorService;
+
+  @EJB
+  private CategoryService categoryService;
+
+//  @EJB
+//  private YouTubeCrawlerService ytService;
+  private Long authorId;
+
+  private Long categoryId;
 
   @Override
   public PlaylistService getService() {
@@ -31,7 +45,14 @@ public class PlaylistController extends BaseController<Playlist, PlaylistService
 
   @Override
   public String edit() {
+    Author author = authorService.find(authorId);
+    Category category = categoryService.find(categoryId);
+
+    item.setAuthor(author);
+    item.setCategory(category);
+
     super.edit();
+
     this.item = new Playlist();
     return Pages.ADMIN_PLAYLISTS;
   }
@@ -43,19 +64,35 @@ public class PlaylistController extends BaseController<Playlist, PlaylistService
     return Pages.ADMIN_PLAYLISTS;
   }
 
-  public String importPlaylist() {
-    this.item = ytService.createPlaylistByCode(item.getCode());
-    System.out.println("Item");
-    System.out.println("Name: " + item.getName());
-    System.out.println("Code: " + item.getCode());
-    System.out.println("Author: " + item.getAuthor());
-    System.out.println("Category: " + item.getCategory());
-    for (Video video : item.getVideoList()) {
-      System.out.println("Video");
-      System.out.println("Title: " + video.getTitle());
-      System.out.println("Code: " + video.getCode());
-    }
-    return Pages.ADMIN_PLAYLISTS;
-
+//  public String importPlaylist() {
+//    this.item = ytService.createPlaylistByCode(item.getCode());
+//    System.out.println("Item");
+//    System.out.println("Name: " + item.getName());
+//    System.out.println("Code: " + item.getCode());
+//    System.out.println("Author: " + item.getAuthor());
+//    System.out.println("Category: " + item.getCategory());
+//    for (Video video : item.getVideoList()) {
+//      System.out.println("Video");
+//      System.out.println("Title: " + video.getTitle());
+//      System.out.println("Code: " + video.getCode());
+//    }
+//    return Pages.ADMIN_PLAYLISTS;
+//
+//  }
+  public Long getCategoryId() {
+    return categoryId;
   }
+
+  public void setCategoryId(Long categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public Long getAuthorId() {
+    return authorId;
+  }
+
+  public void setAuthorId(Long authorId) {
+    this.authorId = authorId;
+  }
+
 }
