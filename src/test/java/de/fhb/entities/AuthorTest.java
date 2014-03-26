377030fbb527f8c093c5f22a4c95e973ea3711b7
Benny Neugebauer <bn@bennyn.de>
2014-03-26 18:14:09 +0100

    Jackson test case in properties file. BLOG

--- src/test/java/de/fhb/entities/AuthorTest.java
+++ src/test/java/de/fhb/entities/AuthorTest.java
@@ -1,6 +1,9 @@
 package de.fhb.entities;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Properties;
 import org.codehaus.jackson.map.ObjectMapper;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
@@ -9,22 +12,34 @@ import org.junit.Test;
 public class AuthorTest {
 
   private static ObjectMapper mapper;
+  private static Properties properties;
 
   public AuthorTest() {
   }
 
   @BeforeClass
-  public static void setUpClass() {
+  public static void setUpClass() throws IOException {
+    // Test cases
+    String path = "AuthorTest.properties";
+    InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+    properties = new Properties();
+    properties.load(in);
+
+    // Jackson mapper
     mapper = new ObjectMapper();
   }
 
   @Test
-  public void testJacksonMapping() throws IOException {
+  /**
+   * @see
+   * http://stackoverflow.com/questions/3891375/how-to-read-a-text-file-resource-into-java-unit-test
+   */
+  public void testJacksonMapping() throws IOException, URISyntaxException {
     Author instance = new Author("Benny Neugebauer");
     instance.setId(1L);
 
-    String expected = "{\"id\":1,\"name\":\"Benny Neugebauer\",\"description\":\"\",\"website\":\"\",\"channelUrl\":\"\"}";
     String actual = mapper.writeValueAsString(instance);
+    String expected = properties.getProperty("testJacksonMapping");
 
     assertEquals(expected, actual);
   }
