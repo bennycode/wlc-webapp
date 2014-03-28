5b725366ddc9fbed09283c262cc05590b74a496e
Yserz <michael.koppen@googlemail.com>
2014-03-28 15:02:26 +0100

    - merge

---
+++ src/main/java/de/fhb/view/forms/CategoryFormModel.java
@@ -0,0 +1,48 @@
+package de.fhb.view.forms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CategoryFormModel extends FormModel {
+
+  public CategoryFormModel() {
+    super.PROPERTY_ORDER = new String[]{
+      "id",
+      "name",
+      "slug",
+      "color",
+      "playlists",
+      "created",
+      "lastModified"
+    };
+  }
+
+  @Override
+  public FormInput[] parseProperties(Map<String, Class<?>> properties) {
+    int size = PROPERTY_ORDER.length;
+    HashMap<String, FormInput> inputs = new HashMap<>(size);
+    FormInput[] formFields = new FormInput[size];
+
+    // Parse each property and collect them in a map
+    for (Map.Entry<String, Class<?>> property : properties.entrySet()) {
+      FormInput input = new FormInput(property);
+
+      String key = input.getKey();
+
+      if (key.equals("id") || key.equals("created") || key.equals("lastModified")) {
+        input.setReadOnly(true);
+      }
+
+      inputs.put(key, input);
+    }
+
+    // Sort properties according to "PROPERTY_ORDER"
+    int i = 0;
+    for (String orderedKey : PROPERTY_ORDER) {
+      formFields[i] = inputs.get(orderedKey);
+      i++;
+    }
+
+    return formFields;
+  }
+}
---
+++ src/test/java/de/fhb/entities/AuthorTest.java
@@ -0,0 +1,42 @@
+package de.fhb.entities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Properties;
+import org.codehaus.jackson.map.ObjectMapper;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AuthorTest {
+
+  private static ObjectMapper mapper;
+  private static Properties properties;
+
+  public AuthorTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() throws IOException {
+    // Test cases
+    String path = "AuthorTest.properties";
+    InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+    properties = new Properties();
+    properties.load(in);
+
+    // Jackson mapper
+    mapper = new ObjectMapper();
+  }
+
+  @Test
+  public void testJacksonMapping() throws IOException, URISyntaxException {
+    Author instance = new Author("Benny Neugebauer");
+    instance.setId(1L);
+
+    String actual = mapper.writeValueAsString(instance);
+    String expected = properties.getProperty("testJacksonMapping");
+
+    assertEquals(expected, actual);
+  }
+}
