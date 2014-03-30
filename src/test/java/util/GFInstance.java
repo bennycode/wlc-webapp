e0babdef45481daa49dd2aebd2b14524a0602068
Yserz <michael.koppen@googlemail.com>
2014-03-30 18:41:09 +0200

    - configured embeddable glassfish connection pool and jdbc resource with commandrunner and maven properties

--- src/test/java/util/GFInstance.java
+++ src/test/java/util/GFInstance.java
@@ -8,8 +8,11 @@ package util;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.glassfish.embeddable.CommandResult;
+import org.glassfish.embeddable.CommandRunner;
 import org.glassfish.embeddable.Deployer;
 import org.glassfish.embeddable.GlassFish;
+import org.glassfish.embeddable.GlassFishException;
 import org.glassfish.embeddable.GlassFishProperties;
 import org.glassfish.embeddable.GlassFishRuntime;
 import org.glassfish.embeddable.archive.ScatteredArchive;
@@ -41,6 +44,10 @@ public class GFInstance {
     Logger.getLogger("javax.enterprise.system").setLevel(Level.WARNING);
 
     glassfish.start();
+
+    createJDBCConnectionPool(glassfish);
+    createJDBCResourceJNDI(glassfish);
+
     deployer = glassfish.getDeployer();
     archive = new ScatteredArchive(APP_NAME, ScatteredArchive.Type.WAR);
     archive.addClassPath(new File("target", "embedded-classes"));
@@ -49,6 +56,76 @@ public class GFInstance {
 
   public static void stopInstance() throws Exception {
     glassfish.dispose();
+  }
+
+  public static void createJDBCResourceJNDI(GlassFish glassfish) throws GlassFishException {
+    CommandRunner commandRunner = glassfish.getCommandRunner();
+    CommandResult result = commandRunner.run(
+            "create-jdbc-resource",
+            "--connectionpoolid=mysql_welovecoding_welovecodingPool",
+            "--enabled=true",
+            /*"--description description", */
+            /*"--property (property=value):name=value*", */
+            /*"--target target", */
+            "jdbc/welovecoding");
+
+    System.out.println("createJDBCResourceJNDI-Result: " + result.getOutput());
+    System.out.println(result.getFailureCause());
+  }
+
+  public static void createJDBCConnectionPool(GlassFish glassfish) throws GlassFishException {
+    CommandRunner commandRunner = glassfish.getCommandRunner();
+
+    String jdbcUsername = System.getProperty("jdbcUsername", "welovecoding");
+    String jdbcPassword = System.getProperty("jdbcPassword", "123456");
+    String jdbcURL = System.getProperty("jdbcURL", "jdbc:mysql://localhost:3306/welovecoding");
+    jdbcURL = jdbcURL.replaceAll(":", "\\\\:");
+
+    CommandResult result = commandRunner.run(
+            "create-jdbc-connection-pool",
+            "--datasourceclassname=com.mysql.jdbc.jdbc2.optional.MysqlDataSource",
+            "--restype=javax.sql.DataSource",
+            "--steadypoolsize=8",
+            "--maxpoolsize=32",
+            "--maxwait=60000",
+            "--poolresize=2",
+            "--idletimeout=170" /*
+             ,"--initsql=initsqlstring"
+             ,"--isolationlevel=isolationlevel"
+             */, "--isisolationguaranteed=true",
+            "--isconnectvalidatereq=false",
+            "--validationmethod=auto-commit" /*
+             ,"--validationtable=validationtable"
+             */, "--failconnection=false",
+            "--allownoncomponentcallers=false",
+            "--nontransactionalconnections=false",
+            "--validateatmostonceperiod=0",
+            "--leaktimeout=0",
+            "--leakreclaim=false",
+            "--statementleaktimeout=0",
+            "--statementleakreclaim=false",
+            "--creationretryattempts=0",
+            "--creationretryinterval=10" /*
+             ,"--sqltracelisteners=sqltracelisteners,sqltracelisteners"
+             */, "--statementtimeout=-1",
+            "--lazyconnectionenlistment=false",
+            "--lazyconnectionassociation=false",
+            "--associatewiththread=false" /*
+             ,"--driverclassname=jdbcdriverclassname"
+             */, "--matchconnections=false",
+            "--maxconnectionusagecount=0",
+            "--ping=false",
+            "--pooling=true",
+            "--statementcachesize=0" /*
+             ,"--validationclassname=validationclassname"
+             */, "--wrapjdbcobjects=false" /*
+             ,"--description description"
+             */, "--property=user=" + jdbcUsername + ":password=" + jdbcPassword + ":url=" + jdbcURL /*
+             ,"--target=target"
+             */, "mysql_welovecoding_welovecodingPool");
+
+    System.out.println("createJDBCConnectionPool-Result: " + result.getOutput());
+    System.out.println(result.getFailureCause());
   }
 
 }
