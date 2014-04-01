66d6f73116e2c116b1d120bba5a75e5b93be7abd
Benny Neugebauer <bn@bennyn.de>
2014-04-01 12:03:14 +0200

    Moved cluttered strings to a centralized place.

---
+++ src/main/java/de/fhb/navigation/Packages.java
@@ -0,0 +1,11 @@
+package de.fhb.navigation;
+
+public class Pages {
+
+  public static final String ADMIN_AUTHORS = "/admin/authors";
+  public static final String ADMIN_CATEGORY = "/admin/categories";
+  public static final String ADMIN_INDEX = "/admin/index";
+  public static final String ADMIN_PLAYLISTS = "/admin/playlists";
+  public static final String ADMIN_VIDEOS = "/admin/videos";
+
+}
