d19bc2f21e676b4f9e8f4a2c2ac14a02de969979
Benny Neugebauer <bn@bennyn.de>
2014-04-26 22:42:56 +0200

    Changed text size limit from 255 to 1024.

--- src/main/java/com/welovecoding/entities/Playlist.java
+++ src/main/java/com/welovecoding/entities/Playlist.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -46,11 +47,13 @@ public class Playlist extends BaseEntity {
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "playlist")
   private List<Video> videos;
 
+  @Column(length = 1024)
+  @Size(min = 0, max = 1024)
   private String code;
 
   @Embedded @Enumerated(EnumType.STRING)
   private LanguageCode languageCode;
-  
+
   private String description;
 
   private boolean enabled;
