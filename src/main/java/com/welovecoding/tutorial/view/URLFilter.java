5b5414ee5bbea3ce848083de81f1c94a0a2b0c87
Yserz <michael.koppen@googlemail.com>
2014-06-28 17:53:59 +0200

    - now really fixed locales ;)

--- src/main/java/com/welovecoding/tutorial/view/URLFilter.java
+++ src/main/java/com/welovecoding/tutorial/view/URLFilter.java
@@ -16,7 +16,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,7 +23,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Michael Koppen
  */
-@WebFilter(filterName = "URLFilter", urlPatterns = {"/*"})
+//@WebFilter(filterName = "URLFilter", urlPatterns = {"/*"})
 public class URLFilter implements Filter {
 
   // The filter configuration object we are associated with.  If
