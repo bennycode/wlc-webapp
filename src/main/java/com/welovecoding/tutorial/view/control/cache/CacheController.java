c351ca63b3dd5f0ddf8c97bd7ce3abdee288988f
Yserz <michael.koppen@googlemail.com>
2014-07-11 02:05:23 +0200

    - small adjustments for cache-statistics

--- src/main/java/com/welovecoding/tutorial/view/control/cache/CacheController.java
+++ src/main/java/com/welovecoding/tutorial/view/control/cache/CacheController.java
@@ -37,25 +37,25 @@ public class CacheController {
     List<Statistic> hits = statService.findAllByType(CacheDidHitStatistic.class);
 
     List<Statistic> dailyHitList = Lists.newArrayList();
-    Date today = cutTillDay(new Date(new Date().getTime() - 82800000));
+    Date today = cutTillDay(new Date(new Date().getTime() - (86400000 * 3)));
     System.out.println("Creating new dailyHits for Date " + today);
     Statistic dailyHits = new Statistic("Hits", today, 1, TimeUnit.HOURS, 0);
     for (Statistic statistic : hits) {
-      System.out.println("Watching for stats in between " + dailyHits.getFromDate() + " and " + dailyHits.getToDate());
-      System.out.println("Hit: \n"
-              + "ID: " + statistic.getId() + "\n"
-              + "Name: " + statistic.getName() + "\n"
-              + "Date: " + statistic.getFromDate() + "\n"
-              + "");
+//      System.out.println("Watching for stats in between " + dailyHits.getFromDate() + " and " + dailyHits.getToDate());
+//      System.out.println("Hit: \n"
+//              + "ID: " + statistic.getId() + "\n"
+//              + "Name: " + statistic.getName() + "\n"
+//              + "Date: " + statistic.getFromDate() + "\n"
+//              + "");
 
       if (statistic.getFromDate().before(dailyHits.getFromDate())) {
         //skipping old stat
-        System.out.println("skipping cuz statistic to old");
+//        System.out.println("skipping cuz statistic to old");
         continue;
       }
       if (statistic.getFromDate().after(dailyHits.getToDate())) {
         // fill next hour
-        System.out.println("Next Hour");
+//        System.out.println("Next Hour");
         dailyHitList.add(dailyHits);
         dailyHits = new Statistic("Hits", dailyHits.getToDate(), 1, TimeUnit.HOURS, 0);
       }
@@ -63,13 +63,12 @@ public class CacheController {
       //between
       if (dailyHits.getFromDate().before(statistic.getFromDate()) && dailyHits.getToDate().after(statistic.getToDate())) {
 
-        System.out.println("Counting up for stat with ID " + statistic.getId() + " name " + statistic.getName() + " and date " + statistic.getFromDate());
-
+//        System.out.println("Counting up for stat with ID " + statistic.getId() + " name " + statistic.getName() + " and date " + statistic.getFromDate());
         dailyHits.setHits(dailyHits.getHits() + 1);
       } else {
-        System.out.println("ERROR");
+//        System.out.println("ERROR");
       }
-      System.out.println("\n\n");
+//      System.out.println("\n\n");
     }
     dailyHitList.add(dailyHits);
     System.out.println("Done");
@@ -117,23 +116,17 @@ public class CacheController {
   }
 
   private static Date cutTillDay(Date date) {
-    System.out.println("given Date: " + date);
     Date newDate = new Date(date.getYear(), date.getMonth(), date.getDate());
-    System.out.println("resulting Date: " + newDate);
     return newDate;
   }
 
   private static Date cutTillHour(Date date) {
-    System.out.println("given Date: " + date);
     Date newDate = new Date(date.getYear(), date.getMonth(), date.getDate(), date.getHours(), 0);
-    System.out.println("resulting Date: " + newDate);
     return newDate;
   }
 
   private static Date cutTillMinute(Date date) {
-    System.out.println("given Date: " + date);
     Date newDate = new Date(date.getYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), 0);
-    System.out.println("resulting Date: " + newDate);
     return newDate;
   }
 }
