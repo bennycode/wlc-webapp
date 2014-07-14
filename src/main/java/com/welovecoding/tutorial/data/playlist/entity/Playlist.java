882c4d59377bd16e691d117a48d09eaff571a989
Yserz <michael.koppen@googlemail.com>
2014-07-14 15:06:30 +0200

    - fixes #92 NEEDS DATABASE-UPDATE

--- src/main/java/com/welovecoding/tutorial/data/playlist/entity/Playlist.java
+++ src/main/java/com/welovecoding/tutorial/data/playlist/entity/Playlist.java
@@ -21,6 +21,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -53,6 +54,7 @@ public class Playlist extends BaseEntity {
   private Author author;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "playlist", orphanRemoval = true)
+  @OrderColumn(name = "ORDERING")
   private List<Video> videos;
 
   private String code;
